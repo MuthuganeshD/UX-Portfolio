11010d376f1b2312f6dcb91b9bf9a656572dd229
Muthuganesh D <126790276+MuthuganeshD@users.noreply.github.com>
2023-10-15 20:12:00 +0530

    Add files via upload
    Hello everyone, this is my Phase 1 Normal task in CodersCave. 
    Here, I have created my portfolio. 
    I have changed the image to a generic user picture for security reasons.

---
+++ uxPortfoilo/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Your UX Portfolio</title>
+</head>
+
+<style>
+    .link{
+        text-decoration: none;
+        color: aqua;
+    }
+    .link:hover{
+        color:#4BD593;
+    }
+</style>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <div class="hero">
+            <h1>Muthuganesh D</h1>
+            <p>UX Designer</p>
+        </div>
+    </section>
+        <!-- Inside the .hero section or wherever you want to display the image -->
+<div class="hero">
+    <img src="user.jpeg" alt="MuthuganeshD" class="square-image">
+    
+</div>
+    
+
+    <section id="about">
+        <div class="about-me">
+            <h2>About Me</h2>
+            <p>I am looking for a challenging job in a
+reputed organization that provide me
+with a range of goals and job
+objectives.</p>
+        </div>
+    </section>
+
+
+    <section id="about">
+        <div class="about-me">
+            <p><h2>ACADEMIC PROFILE</h2></p>
+            <p>Bachelor of Technology Information
+Technology with 8.49 CGPA (upto
+4th semester) at M.Kumarasamy
+College of Engineering.</p><br>
+<p>Higher Secondary School Leaving
+Certificate (2021) with 90.38% at
+Cheran Matric Higher Secondary
+School.
+</p><br>
+<p>Secondary School Leaving
+Certificate (2019) with 87.4% at
+Cheran Matric Higher Secondary
+School.</p>
+
+        </div>
+
+
+    </section>
+
+    <section id="projects">
+        <!-- Your projects will go here -->
+        <div class="about-me">
+            <p>
+                <h2>PROJECTS</h2>
+            </p>
+            <p><h4>AgriTrade Hub</h4></p>
+
+            <div class="hero">
+    <img src="WhatsApp Image 2023-10-10 at 1.33.32 PM.jpeg" alt="MuthuganeshD" class="square-image" style="width: 100%; max-width: 600px; height: auto; object-fit: cover; border-radius: 10px;">
+    
+</div>
+            <p>Welcome to the AgriTrade Hub, a friendly online place that connects farmers 
+and buyers like never before! In simple terms, it's a website where farmers from 
+different places can show what they grow, and buyers can easily buy these 
+products directly from them. Farmers get to share details about their products 
+and themselves, and buyers can check everything out before making a purchase. 
+Plus, buyers can leave feedback, helping everyone trust each other more. This 
+platform cuts out the middlemen, making sure farmers get fair prices for their 
+hard work and buyers get good deals. With easy-to-use features and safe 
+payment methods, the AgriTrade Hub is changing the way we buy and sell farm 
+products, making farming more sustainable and helping communities grow. </p>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="contact-form">
+            <!-- Contact form goes here -->
+            <p><h2>CONTACT</h2></p>
+            <p>Mail: <a href="mailto:muthuganesh1046@gmail.com" class="link" >E-mail id</a><br>
+            Linkedin: <a href="https://www.linkedin.com/in/muthuganesh-d-b11886268/" target="_blank" class="link">linkedin</a>
+        </p>
+        </div>
+    </section>
+
+    <script src="script.js"></script>
+
+</body>
+
+</html>
---
+++ uxPortfoilo/script.js
@@ -0,0 +1,9 @@
+// // JavaScript code for changing the background color dynamically
+// function changeBackgroundColor() {
+//     const colors = ['#3498db', '#e74c3c', '#2980b9', '#c0392b', '#16a085', '#d35400', '#2ecc71', '#f39c12']; // List of colors
+//     const randomColor = colors[Math.floor(Math.random() * colors.length)]; // Get a random color from the list
+//     document.body.style.backgroundColor = randomColor; // Apply the random color to the body background
+// }
+
+// // Call the function when the page is loaded
+// window.onload = changeBackgroundColor;
